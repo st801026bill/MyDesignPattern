2e852a4468a8970da8934512c7666e2297839827
Bill Chou <st801026bill@hotmail.com.tw>
2019-10-10 00:53:45 +0800

    iterator pattern : 增加 CafeMenu(Hashtable iterator)

--- src/dp/TestDesignPattern.java
+++ src/dp/TestDesignPattern.java
@@ -17,7 +17,6 @@ import dp.factorypattern.TestFactoryPatternByFactory;
 import dp.factorypattern.TestFactoryPatternBySimple;
 import dp.iteratorpattern_compositepattern.TestIteratorPattern1;
 import dp.iteratorpattern_compositepattern.TestIteratorPattern2;
-import dp.iteratorpattern_compositepattern.TestIteratorPattern3;
 import dp.observerpattern.TestObserverPatternByCustom;
 import dp.observerpattern.TestObserverPatternByJava;
 import dp.singletonpattern.TestSingletonPattern;
@@ -47,7 +46,7 @@ public class TestDesignPattern {
             System.out.println(" 7:轉接器模式");
             System.out.println(" 8:表象模式");
             System.out.println(" 9A:樣板方法模式, 9B:樣板方法模式(Hook), 9C:樣板方法模式(CompareTo!?)");
-            System.out.println(" 10A:反覆器模式(自訂), 10B:反覆器模式(import jdk), 10C:反覆器模式(Hashtable iterator), ");
+            System.out.println(" 10A:反覆器模式(自訂), 10B:反覆器模式(import jdk)");
             System.out.print("請輸入代號:");
             String mode = sc.nextLine();
             System.out.println("----------------------------------------");
@@ -89,8 +88,6 @@ public class TestDesignPattern {
                 case "10A": TestIteratorPattern1.main(args);
                     break;
                 case "10B": TestIteratorPattern2.main(args);
-                    break;
-                case "10C": TestIteratorPattern3.main(args);
                     break;
                 case "0":
                     return;
